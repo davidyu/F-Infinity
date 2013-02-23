653981ba031d0d130be9fb8e91f3c4005e5327e2
Desktop <lewen.yu@gmail.com>
2013-02-23 13:40:21 -0800

    trying Heroku

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  var message = 'TO INFINITY AND BEYOND<br/>that means it\'s working<br/>app_port:' + process.env.VMC_APP_PORT;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('TO INFINITY AND BEYOND MOTHERFUCKER<br/>that means it\'s working');
+  res.end( message );
 }).listen(process.env.VMC_APP_PORT || 1337, null);
+
+var io = require('socket.io').listen(http);
+
+//io.set('log level', 1);
+
+/*io.sockets.on( 'connection', function( socket ) {
+
+    //start listening for keydown events??
+
+})*/
--- finfinity/app.js
+++ finfinity/app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('TO INFINITY AND BEYOND MOTHERFUCKER<br/>that means it\'s working');
+  res.end('TO INFINITY AND BEYOND MOTHERFUCKER<br/>that means it\'s working' + '<br/>app_port:' + process.env.VMC_APP_PORT);
 }).listen(process.env.VMC_APP_PORT || 1337, null);
 
 var io = require('socket.io').listen(http);
