3307f6c6ea25a64801baa7318d5507d737beb7b3
Desktop <lewen.yu@gmail.com>
2013-02-23 14:28:52 -0800

    working socket.io

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  var message = 'TO INFINITY AND BEYOND<br/>that means it\'s working<br/>app_port:' + process.env.VMC_APP_PORT;
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end( message );
-}).listen(process.env.VMC_APP_PORT || 1337, null);
+var app_port = 8080;
 
-var io = require('socket.io').listen(http);
+var app = require('http').createServer( handler );
+var io = require('socket.io').listen(app);
+
+function handler(req, res) {
+    var message = 'TO INFINITY AND BEYOND<br/>that means it\'s working<br/>app_port:' + app_port;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end( message );
+}
+
+app.listen( app_port || 1337, null );
+
+
 
 //io.set('log level', 1);
 
